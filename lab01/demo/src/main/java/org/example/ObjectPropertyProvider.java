19a0043fe4f428de9c30d1450d05b675569afc96
iraxus1 <piotrwiktor31@interia.pl>
2021-10-13 16:49:27 +0200

    zad

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class ObjectPropertyProvider {
 
@@ -59,23 +60,37 @@ public class ObjectPropertyProvider {
 
     public List<Field> getFieldsForPublicProperties(Class<?> clazz){
 
-        List<> setterList = 
-        ArrayList<Method> mList = new ArrayList<>();
-        Method[] methods = clazz.getMethods();
-        for (Method method: methods) {
-            if(Modifier.toString(method.getModifiers())=="public") {
-                if (method.getName().startsWith("is") || method.getName().startsWith("get")||method.getName().startsWith("set")) {
-                    if(method.getParameterTypes().length == 0||method.getParameterTypes().length == 1)
-                    {
-                        if(!void.class.equals(method.getReturnType())||void.class.equals(method.getReturnType()))
-                        {
-                            if(method.getName().contains())
-                            mList.add(method);
-                        }
-                    }
+        List<Method> setterList = getPublicGetters(clazz);
+        List<Method> getterList = getPublicSetters(clazz);
+        ArrayList<Field> mList = new ArrayList<>();
+        Field[] fields = clazz.getFields();
+        boolean isInField = false;
+        for (Field field:fields
+             ) {
+            for (Method method: setterList
+                 ) {
+                if(method.getName().toLowerCase(Locale.ROOT).contains(field.getName().toLowerCase(Locale.ROOT))){
+                    isInField = true;
                 }
+
+
             }
+
+            for (Method method: getterList
+            ) {
+                if(method.getName().toLowerCase(Locale.ROOT).contains(field.getName().toLowerCase(Locale.ROOT))){
+                    isInField = true;
+                }
+
+
+            }
+            if(isInField)
+            {
+                mList.add(field);
+            }
+            isInField = false;
         }
+
         return Arrays.stream(clazz.getDeclaredFields()).toList();
 
     }
