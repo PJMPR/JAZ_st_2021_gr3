dc62b11fd5ed7b23e684a8dd33347f20a0d9084b
Krzysztof Paliga <s22463@pjwstk.edu.pl>
2021-10-13 17:01:39 +0200

    13.10

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -1,33 +1,43 @@
 package org.example;
 
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        Method m = Arrays.stream(clazz.getDeclaredMethods()).findFirst().or(null).get();
+        Method[] methods = clazz.getMethods();
+        List<Method> listOfGetters = new ArrayList<Method>();
 
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
-
+        for (Method method : methods) {
+            if (Modifier.toString(method.getModifiers()).equals("Public")){
+                if(method.getName().contains("get")||method.getName().contains("is")){
+                    listOfGetters.add(method);
+                }
+            }
+        }
+        return listOfGetters;
     }
 
-
     public List<Method> getPublicSetters(Class<?> clazz){
+        Method[] methods = clazz.getMethods();
+        List<Method> listOfSetters = new ArrayList<Method>();
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        for (Method method : methods) {
+            if (Modifier.toString(method.getModifiers()).equals("Public")){
+                if(method.getName().contains("set")){
+                    listOfSetters.add(method);
+                }
+            }
+        }
+        return listOfSetters;
     }
 
 
     public List<Field> getFieldsForPublicProperties(Class<?> clazz){
-
-        return Arrays.stream(clazz.getDeclaredFields()).toList();
-
     }
-
-
 }
