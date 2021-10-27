924a88a3b296091622a154d365352d6219b0ada8
Saszqa <ft.tamkun@gmail.com>
2021-10-27 23:33:19 +0200

    donzo

---
+++ src/main/java/org/example/annotations/NotNull.java
@@ -0,0 +1,12 @@
+package org.example.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNull{
+    String messageIsNull () default "field is null";
+}
