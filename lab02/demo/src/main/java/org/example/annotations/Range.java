9cd1d48410b5d0b5a249faca0560c04e90db90de
maciejrudnicki2001 <s22983@pjwstk.edu.pl>
2021-10-28 13:23:57 +0200

    added interfaces

--- lab02/demo/src/main/java/org/example/annotations/Range.java
+++ lab02/demo/src/main/java/org/example/annotations/Range.java
@@ -1,5 +1,6 @@
 package org.example.annotations;
 
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,7 +8,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface NotNull {
-    String message() default "field is null";
-    String message2() default "field is empty";
+public @interface Range {
+    int min();
+    int max();
 }
--- lab02/demo/src/main/java/org/example/annotations/Regex.java
+++ lab02/demo/src/main/java/org/example/annotations/Regex.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface NotNull {
-    String message() default "field is null";
-    String message2() default "field is empty";
+public @interface Regex {
+    String pattern();
+    String message() default "regex error";
 }
