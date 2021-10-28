29d4a83a0423a9e1a86b7ad3d3dc988e66dc52b2
electroluxv2 <electroluxv2@gmail.com>
2021-10-28 18:46:51 +0200

    combinedAnnotationValidator

--- lab02/demo/src/main/java/org/example/validators/AnnotationValidator.java
+++ lab02/demo/src/main/java/org/example/validators/AnnotationValidator.java
@@ -4,28 +4,35 @@ import org.example.annotations.NotNull;
 import org.example.annotations.Range;
 import org.example.annotations.Regex;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface AnnotationValidator {
-    Stream<String> getErrors(Stream<AnnotatedField> fields, Object object);
+    Stream<String> getErrors(List<AnnotatedField> fields, Object object);
 
-    AnnotationValidator notNullAnnotation = (fields, object) -> fields
+    AnnotationValidator notNullAnnotation = (fields, object) -> fields.stream()
             .filter(field -> field.hasAnnotation(NotNull.class))
             .filter(AnnotatedField::isNull)
             .map(field -> field.getAnnotation(NotNull.class))
             .map(NotNull::message);
 
-    AnnotationValidator regexAnnotation = (fields, object) -> fields
+    AnnotationValidator regexAnnotation = (fields, object) -> fields.stream()
             .filter(field -> field.hasAnnotation(Regex.class))
             .filter(AnnotatedField::isString)
             .filter(field -> field.notMatchesRegex(field.getAnnotation(Regex.class).pattern()))
             .map(field -> field.getAnnotation(Regex.class))
             .map(Regex::message);
 
-    AnnotationValidator rangeAnnotation = (fields, object) -> fields
+    AnnotationValidator rangeAnnotation = (fields, object) -> fields.stream()
             .filter(field -> field.hasAnnotation(Range.class))
             .filter(AnnotatedField::isNumber)
             .filter(AnnotatedField::isNotWithinAnnotatedRange)
             .map(field -> field.getAnnotation(Range.class))
             .map(range -> "number is out of range [%d,%d]".formatted(range.min(), range.max()));
+
+    AnnotationValidator combinedAnnotationValidator = (fields, object) -> Stream.of(
+            AnnotationValidator.notNullAnnotation.getErrors(fields, object),
+            AnnotationValidator.regexAnnotation.getErrors(fields, object),
+            AnnotationValidator.rangeAnnotation.getErrors(fields, object)
+    ).flatMap(v -> v);
 }
--- lab02/demo/src/main/java/org/example/validators/Validator.java
+++ lab02/demo/src/main/java/org/example/validators/Validator.java
@@ -1,10 +1,8 @@
 package org.example.validators;
 
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Validator {
 
@@ -14,11 +12,7 @@ public class Validator {
                 .map(field -> new AnnotatedField(field, object))
                 .collect(Collectors.toMap(
                         AnnotatedField::getName,
-                        field -> Stream.of(
-                                AnnotationValidator.notNullAnnotation.getErrors(Stream.of(field), object),
-                                AnnotationValidator.regexAnnotation.getErrors(Stream.of(field), object),
-                                AnnotationValidator.rangeAnnotation.getErrors(Stream.of(field), object)
-                        ).flatMap(i -> i).toList()
+                        field -> AnnotationValidator.combinedAnnotationValidator.getErrors(List.of(field), object).toList()
                 ));
 
         notValidFields.values().removeIf(List::isEmpty);
