ead90bf5ca8231a7a37c023c1012853551703ec6
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2021-11-08 22:30:08 +0100

    Add files via upload

--- lab03/demo/src/main/java/org/example/queries/Criteria/PageCriteria.java
+++ lab03/demo/src/main/java/org/example/queries/Criteria/PageCriteria.java
@@ -7,9 +7,10 @@ public class PageCriteria implements Criteria{
     @Override
     public void meetCriteria(Results results, SearchParameters searchParameters) {
         if (searchParameters.getPage() != null) {
-            int s = Math.min(searchParameters.getPage().getSize()*(searchParameters.getPage().getPageNumber() - 1), results.getItems().size());
-            int s2 = Math.min(searchParameters.getPage().getSize()*(searchParameters.getPage().getPageNumber()), results.getItems().size());
-            results.setItems(results.getItems().subList(s, s2));
+            searchParameters.getPage().setS(Math.min(searchParameters.getPage().getSize()*(searchParameters.getPage().getPageNumber() - 1), results.getItems().size()));
+            searchParameters.getPage().setS2(Math.min(searchParameters.getPage().getSize()*(searchParameters.getPage().getPageNumber()), results.getItems().size()));
+
+            results.setItems(results.getItems().subList(searchParameters.getPage().getS(), searchParameters.getPage().getS2()));
             results.setCurrentPage(searchParameters.getPage().getPageNumber());
             results.setPages((int) Math.ceil((double) results.getItems().size() / searchParameters.getPage().getSize()));
         }
