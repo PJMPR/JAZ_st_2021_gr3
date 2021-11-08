d350ef7500c10cf6d4c88c2e701e31f803476caf
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2021-11-08 22:30:35 +0100

    Add files via upload

--- lab03/demo/src/main/java/org/example/queries/search/Page.java
+++ lab03/demo/src/main/java/org/example/queries/search/Page.java
@@ -4,6 +4,24 @@ public class Page {
 
     private int size;
     private int pageNumber;
+    private int s2;
+    private int s;
+
+    public int getS2() {
+        return s2;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS2(int s2) {
+        this.s2 = s2;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
 
     public Page(int size, int pageNumber) {
         this.size = size;
