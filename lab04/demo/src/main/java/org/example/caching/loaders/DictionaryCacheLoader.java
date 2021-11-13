1ea9af24ca5ea1bc27c166dfc6111a54dbcf269b
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2021-11-13 22:38:13 +0100

    update

--- lab04/demo/src/main/java/org/example/caching/loaders/DictionaryCacheLoader.java
+++ lab04/demo/src/main/java/org/example/caching/loaders/DictionaryCacheLoader.java
@@ -1,9 +1,28 @@
 package org.example.caching.loaders;
 
+import org.example.caching.Cache;
+import org.example.caching.providers.DataProvider;
+import org.example.caching.providers.FileDataProvider;
+import org.example.model.Dictionary;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DictionaryCacheLoader implements CacheLoader{
 
-    @Override
-    public void load() {
+    private final Cache cache = Cache.getInstance();
+    private final List<DataProvider> dataProviderList = List.of(new FileDataProvider());
 
+    public void load(){
+        List<Dictionary> dictionaryList = new ArrayList<>();
+        dataProviderList.forEach(dataProvider -> {
+            try {
+                dictionaryList.addAll(dataProvider.dataProviderList());
+                cache.add("dictionaries", dictionaryList);
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+        });
     }
 }
