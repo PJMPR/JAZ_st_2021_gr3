d7e65357a1d23c411391cd526f9721915ec92134
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2021-11-13 22:36:10 +0100

    update

--- lab04/demo/src/main/java/org/example/caching/providers/FileDataProvider.java
+++ lab04/demo/src/main/java/org/example/caching/providers/FileDataProvider.java
@@ -1,14 +1,41 @@
 package org.example.caching.providers;
 
-
 import org.example.model.Dictionary;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class FileDataProvider implements DataProvider{
+public class FileDataProvider implements DataProvider {
 
-    @Override
-    public List<Dictionary> dataProviderList() {
-        return null;
+    private List<Dictionary> data() throws FileNotFoundException {
+        String filePath = "src/dictionaries.csv";
+        Scanner scanner = new Scanner(new File(filePath));
+        List<Dictionary> dictionaryList = new ArrayList<>();
+        Dictionary dictionary = new Dictionary();
+
+        while (scanner.hasNext()) {
+            String[] strings = scanner.nextLine().split(",");
+
+            dictionary.setId(Integer.parseInt(strings[0]));
+            dictionary.setIntKey(Integer.parseInt(strings[1]));
+            dictionary.setStringKey(strings[2]);
+            dictionary.setValue(strings[3]);
+            dictionary.setDictionaryName(strings[4]);
+
+            dictionaryList.add(new Dictionary(
+                    dictionary.getId(),
+                    dictionary.getIntKey(),
+                    dictionary.getStringKey(),
+                    dictionary.getValue(),
+                    dictionary.getDictionaryName()));
+        }
+        return dictionaryList;
+    }
+
+    public List<Dictionary> dataProviderList() throws FileNotFoundException {
+        return data();
     }
 }
