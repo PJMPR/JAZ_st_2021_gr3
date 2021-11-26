b910b6d7e00cd14371f3df3f0adb4d43542971a8
cytg <cytgrzechu@gmail.com>
2021-11-26 23:57:03 +0100

    added new springboot project template

---
+++ lab06/creditSystem/src/main/java/com/example/creditsystem/CreditSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditSystemApplication.class, args);
+    }
+
+}
---
+++ lab06/creditSystem/src/test/java/com/example/creditsystem/CreditSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
