d331d785cbe414231373290c2c38796a7196ee12
electroluxv2 <electroluxv2@gmail.com>
2021-12-04 06:03:15 +0100

    ---

--- lab07/demo/demo/src/main/java/com/example/demo/controllers/CustomerRankingController.java
+++ lab07/demo/demo/src/main/java/com/example/demo/controllers/CustomerRankingController.java
@@ -2,6 +2,13 @@ package com.example.demo.controllers;
 
 import com.example.demo.repositories.CustomerRecords;
 import com.example.demo.repositories.CustomerRepository;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.xy.XYBarDataset;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,8 +31,46 @@ public class CustomerRankingController {
     }
 
     @GetMapping("bySpentMoney")
-    public ResponseEntity<List<CustomerRecords.CustomerRankingBySpentMoneyEntry>> bySpentMoney(@RequestParam("chart") final Optional<String> chart) {
-        return new ResponseEntity<>(repository.get10CustomersByMostSpentMoney(), HttpStatus.OK);
+    public ResponseEntity<List<CustomerRecords.CustomerRankingBySpentMoneyEntry>> bySpentMoney(HttpServletResponse response, @RequestParam("chart") final Optional<String> chart) throws IOException {
+
+        final var list = repository.get10CustomersByMostSpentMoney();
+
+        if (chart.isPresent()) {
+            if (chart.get().equals("pie")) {
+                DefaultPieDataset<String> pieDataset = new DefaultPieDataset<>();
+
+                for (var item : list) {
+                    pieDataset.setValue(item.getCustomer().firstName() + " " + item.getCustomer().lastName(), item.getSpent().doubleValue());
+                }
+
+                JFreeChart pieChart3D = ChartFactory.createPieChart("bySpentMoney", pieDataset, true, true, true);
+
+                ChartUtils.writeChartAsPNG(response.getOutputStream(), pieChart3D, 800, 600);
+                response.flushBuffer();
+            } else if (chart.get().equals("bar")) {
+                DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+                for (var item : list) {
+                    dataset.setValue(item.getSpent().doubleValue(), item.getCustomer().firstName() + " " + item.getCustomer().lastName(), "users");
+                }
+
+                JFreeChart barChart = ChartFactory.createBarChart(
+                        "bySpentMoney",
+                        "users",
+                        "bySpentMoney",
+                        dataset,
+                        PlotOrientation.VERTICAL,
+                        true,
+                        true,
+                        false
+                );
+
+                ChartUtils.writeChartAsPNG(response.getOutputStream(), barChart, 800, 600);
+                response.flushBuffer();
+            }
+        }
+
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("byWatchedMovies")
