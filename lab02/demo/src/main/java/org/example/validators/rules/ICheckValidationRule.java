3f4d8f4609d7e038ba0e8253b513120615f5c9e4
electroluxv2 <electroluxv2@gmail.com>
2021-12-05 22:36:14 +0100

    Lab 06

--- lab02/demo/src/main/java/org/example/validators/rules/ICheckValidationRule.java
+++
@@ -1,7 +0,0 @@
-package org.example.validators.rules;
-
-import org.example.validators.ValidationResult;
-
-public interface ICheckValidationRule {
-    void ValidateRule(ValidationResult validationResult);
-}
