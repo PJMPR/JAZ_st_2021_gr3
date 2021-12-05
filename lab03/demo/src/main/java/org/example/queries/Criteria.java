96e7e36fbecfc29298296617814a6830724c6a0b
electroluxv2 <electroluxv2@gmail.com>
2021-12-05 22:25:31 +0100

    Lab 02

--- lab03/demo/src/main/java/org/example/queries/Criteria.java
+++
@@ -1,9 +0,0 @@
-package org.example.queries;
-
-import org.example.queries.results.Results;
-import org.example.queries.search.SearchParameters;
-
-public interface Criteria {
-    void setParameters(SearchParameters parameters);
-    void meetCriteria(Results results);
-}
--- lab03/demo/src/main/java/org/example/queries/CriteriaPredicate.java
+++
@@ -1,9 +0,0 @@
-package org.example.queries;
-
-import org.example.model.Person;
-import org.example.queries.search.SearchParameters;
-
-@FunctionalInterface
-public interface CriteriaPredicate {
-    boolean check(Person p, SearchParameters params);
-}
