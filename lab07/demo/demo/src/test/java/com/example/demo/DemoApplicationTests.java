64d1c79cf677962bcf82bdea99451e657b90c631
electroluxv2 <electroluxv2@gmail.com>
2021-12-06 20:01:33 +0100

    One more test

--- lab07/demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ lab07/demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,24 +4,23 @@ import com.example.demo.controllers.ChartMaker;
 import com.example.demo.repositories.records.DatasetEntry;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class DemoApplicationTests {
 
 	HttpServletResponse response = new MockHttpServletResponse();
-
-	@Mock
-	List<DatasetEntry> dataset;
+	List<DatasetEntry> dataset = new ArrayList<>(List.of(new DatasetEntry("key", 12)));
 
 	String title = "title";
 	String exporter = "bar";
@@ -31,5 +30,12 @@ class DemoApplicationTests {
 		ChartMaker mockChartMaker = Mockito.mock(ChartMaker.class);
 		mockChartMaker.export(exporter, response, dataset, title);
 		Mockito.verify(mockChartMaker.BAR.getExporter(), times(1)).export(dataset, title);
+	}
+
+	@Test
+	public void allExportersShouldWork() {
+		for (var exporter : ChartMaker.values()) {
+			assertDoesNotThrow(() -> ChartMaker.export(exporter.name(), response, dataset, title));
+		}
 	}
 }
