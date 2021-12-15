08157e8bd2100b1a350cf36df9c7e3e704461af1
januszewskijanuszek <januszewskijanuszek@gmail.com>
2021-12-15 14:02:03 +0100

    .

---
+++ lab08/demo/demo/src/main/java/com/example/demo/contract/MovieDto.java
@@ -0,0 +1,45 @@
+package com.example.demo.contract;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MovieDto {
+
+    @JsonProperty("original_title")
+    String title;
+    String overview;
+    int id;
+    @JsonProperty("imdb_id")
+    String imdbId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+}
